e892875f1a811d6483a9b5831b1d48d37eee2138
Micah Silverman <micah@afitnerd.com>
2016-08-12 22:49:00 -0400

    Initial Commit

---
+++ src/main/java/com/stormpath/blog/SpringPrimefacesDemo/config/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.stormpath.blog.SpringPrimefacesDemo.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext sc) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        sc.addListener(new ContextLoaderListener(context));
+	}	
+}
---
+++ src/main/java/com/stormpath/blog/SpringPrimefacesDemo/presentation/CustomerBean.java
@@ -0,0 +1,47 @@
+package com.stormpath.blog.SpringPrimefacesDemo.presentation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.stormpath.blog.SpringPrimefacesDemo.model.Customer;
+
+@ManagedBean
+@ViewScoped
+public class CustomerBean {
+	private List<Customer> customers;
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+	
+	@PostConstruct
+	public void setup()  {
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		Customer customer1 = new Customer();
+		customer1.setFirstName("John");
+		customer1.setLastName("Doe");
+		customer1.setCustomerId(123456);
+		
+		customers.add(customer1);
+		
+		Customer customer2 = new Customer();
+		customer2.setFirstName("Adam");
+		customer2.setLastName("Scott");
+		customer2.setCustomerId(98765);
+		
+		customers.add(customer2);
+		
+		Customer customer3 = new Customer();
+		customer3.setFirstName("Jane");
+		customer3.setLastName("Doe");
+		customer3.setCustomerId(65432);
+		
+		customers.add(customer3);
+		this.customers = customers;
+	}	
+}
